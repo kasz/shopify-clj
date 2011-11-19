3b9a64c1c5a7e74dc5bc85358409d8eb8b7194f0
Chris Saunders <chris@christophersaunders.ca>
2011-11-19 15:00:35 -0500

    Initial commit

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject shopify-clj "1.0.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :dependencies [
+                 [org.clojure/clojure "1.2.1"]
+                 [org.clojure/clojure-contrib "1.2.0"]
+                 [clj-http "0.2.4"]
+                 [digest "1.3.0"]
+                 ])
---
+++ test/shopify_clj/test/core.clj
@@ -0,0 +1,6 @@
+(ns shopify-clj.test.core
+  (:use [shopify-clj.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
